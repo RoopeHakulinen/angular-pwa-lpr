a23c7fd06188d58e20a168cf8a1d788db000cc55
Roope Hakulinen <roope.hakulinen@gmail.com>
2018-06-06 12:03:48 +0100

    Final implementation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
+import { FormsModule } from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {ServiceWorkerModule} from '@angular/service-worker';
 import {environment} from '../environments/environment';
+import { TodoService } from './todo.service';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     ServiceWorkerModule.register('/ngsw-worker.js', {
       enabled: environment.production
     })
   ],
-  providers: [],
+  providers: [
+    TodoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
